d1fd2ff784f57dfbbd5f58b4135340084f17ab04
Tostapunk <s.mattia2000@gmail.com>
2017-07-02 08:46:49 +0200

    Xenonify

--- xenonhd.mk
+++ xenonhd.mk
@@ -21,10 +21,20 @@ TARGET_SCREEN_HEIGHT := 960
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := surnia
-PRODUCT_NAME := lineage_surnia
+PRODUCT_NAME := xenonhd_surnia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := surnia
+
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=Tostapunk
+
+ROOT_METHOD=magisk
+
+PRODUCT_PACKAGES += \
+    Adaway \
+    KernelAdiutor \
+    MiXplorer \
+    Snap
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Moto E LTE (2nd gen)"
